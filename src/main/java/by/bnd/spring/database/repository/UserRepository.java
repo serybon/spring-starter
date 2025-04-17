fe9144c1a1d1344537bdfbebc2cd4336bd9b00a2
Siarhei Bondar <58183264+serybon@users.noreply.github.com>
2025-04-17 15:05:52 +0300

    + resource

--- src/main/java/by/bnd/spring/database/repository/UserRepository.java
+++ src/main/java/by/bnd/spring/database/repository/UserRepository.java
@@ -3,13 +3,19 @@ package by.bnd.spring.database.repository;
 import by.bnd.spring.bpp.InjectBean;
 import by.bnd.spring.database.repository.pool.ConnectionPool;
 
+import jakarta.annotation.Resource;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 @ToString
 public class UserRepository {
-    //@InjectBean
-    @Autowired
+//    @InjectBean
+//    @Autowired
+    @Resource(name = "connectionPool1")
     private ConnectionPool connectionPool;
+
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
 }
